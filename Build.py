0ae35e53358fac3c80af46bf7c071c221f1d85bc
ElasticSea <info@elasticsea.com>
2018-07-13 01:55:01 +0200

    Build script in progress, can run tests.

---
+++ Build.py
@@ -0,0 +1,38 @@
+# Build.py --platforms windows ios android
+#          --serial XX-XXXX-XXXX-XXXX-XXXX-XXXX
+#          --username username
+#          --password password
+#          --unitypath "C:\Unitypath"
+#          --projectpath "C:\Projectpath"
+#          --tests
+
+import subprocess
+import argparse
+import time
+
+parser = argparse.ArgumentParser(description='Triggers build for multiple platforms in unity.')
+parser.add_argument('--platforms', nargs='+', help='An optional integer positional argument', required=True)
+parser.add_argument('--serial', help='Unity licence serial number.', required=True)
+parser.add_argument('--username', help='Your account username.', required=True)
+parser.add_argument('--password', help='Your account password.', required=True)
+parser.add_argument('--unitypath', help='Unity engine path.', required=True)
+parser.add_argument('--projectpath', help='Unity project path.', required=True)
+parser.add_argument('--tests', action='store_true', help='Run tests before build.')
+parser.add_argument('--debug', action='store_true', help='Run build in debug mode.')
+args = parser.parse_args()
+
+if args.tests:
+    start_time = time.time()
+    result = subprocess.run([args.unitypath,
+                             "-serial", args.serial,
+                             "-username", args.username,
+                             "-password", args.password,
+                             "-projectPath", args.projectpath,
+                             "-logFile", args.projectpath + "\\log.txt",
+                             "-batchmode",
+                             "-runEditorTests"])
+    if result.returncode != 0:
+        raise ValueError('Test failed aborting build.')
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+print(args.platforms)
